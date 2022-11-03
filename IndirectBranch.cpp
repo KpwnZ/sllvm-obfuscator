d0f3a77c25639a04e9e09d73457d17f81e9e608f
KpwnZ <22996989+KpwnZ@users.noreply.github.com>
2022-11-03 18:44:00 +0800

    Use largest available int type.

--- IndirectBranch.cpp
+++ IndirectBranch.cpp
@@ -11,34 +11,55 @@
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Transforms/Utils/BasicBlockUtils.h"
 #include "llvm/Transforms/Utils/Local.h"
+#include "llvm/IR/DataLayout.h"
 
 namespace sllvm {
 
 struct IndirectBranch : llvm::PassInfoMixin<IndirectBranch> {
+    llvm::Type *largestIntType;
+    
     llvm::PreservedAnalyses run(llvm::Module &M, llvm::ModuleAnalysisManager &MAM) {
+
+        auto datalayout = llvm::DataLayout(&M);
+        largestIntType = datalayout.getLargestLegalIntType(M.getContext());
+        assert(largestIntType != nullptr);
+        
         static std::random_device device;
         static std::mt19937 rng(device());
-        static std::uniform_int_distribution<std::mt19937::result_type> distribution(0xfffff, 0xffffffff);
+        static std::uniform_int_distribution<std::mt19937::result_type> distribution(0x100, 0xfffff);
 
         std::map<llvm::BasicBlock *, size_t> blocks2key;
         std::map<llvm::BasicBlock *, size_t> blocks2index;
         std::vector<llvm::Constant *> globalBlocksArray;
 
         for (auto &F : M) {
+            if (F.getBasicBlockList().size() <= 2) continue;    // we don't need to obfuscate the function with only one and two block
             for (auto &BB : F) {
-                llvm::BasicBlock *b = &BB;
-                if (b == &F.getEntryBlock()) continue;
-                blocks2key[b] = distribution(rng);
+                auto *instr = BB.getTerminator();
+                if (auto *branchInstr = dyn_cast<llvm::BranchInst>(instr)) {
+                    // now get the succesor
+                    auto cnt = branchInstr->getNumSuccessors();
+                    for (unsigned int i = 0; i < cnt; ++i) {
+                        auto *b = branchInstr->getSuccessor(i);
+                        
+                        if(blocks2index.count(b) == 0) {
+                            // add to vector
+                            if (b == &F.getEntryBlock()) continue;
+                            blocks2key[b] = distribution(rng);
 
-                auto *bbPtr = llvm::BlockAddress::get(&BB);
-                auto *bbPtrInt = llvm::ConstantExpr::getPtrToInt(bbPtr, llvm::Type::getInt64Ty(F.getContext()), false);
-                bbPtrInt = llvm::ConstantExpr::getAdd(bbPtrInt, llvm::ConstantInt::get(llvm::Type::getInt64Ty(F.getContext()), blocks2key[b]));
+                            auto *bbPtr = llvm::BlockAddress::get(&BB);
+                            auto *bbPtrInt = llvm::ConstantExpr::getPtrToInt(bbPtr, largestIntType, false);
+                            bbPtrInt = llvm::ConstantExpr::getAdd(bbPtrInt, llvm::ConstantInt::get(largestIntType, blocks2key[b]));
 
-                blocks2index[b] = globalBlocksArray.size();
-                globalBlocksArray.push_back(bbPtrInt);
+                            blocks2index[b] = globalBlocksArray.size();
+                            globalBlocksArray.push_back(bbPtrInt);
+
+                        }
+                    }
+                }
             }
         }
-        auto *arrayType = llvm::ArrayType::get(llvm::Type::getInt64Ty(M.getContext()), globalBlocksArray.size());
+        auto *arrayType = llvm::ArrayType::get(largestIntType, globalBlocksArray.size());
         auto *bArray = llvm::ConstantArray::get(arrayType, llvm::ArrayRef<llvm::Constant *>(globalBlocksArray));
         auto *globBlockArray = new llvm::GlobalVariable(M, arrayType, false, llvm::GlobalValue::LinkageTypes::PrivateLinkage, bArray, "");
         replaceUse(M, blocks2key, globBlockArray, blocks2index);
@@ -64,13 +85,13 @@ struct IndirectBranch : llvm::PassInfoMixin<IndirectBranch> {
                         }
                         llvm::IRBuilder<> builder(branchInstr);
                         
-                        auto *indexGlob = new llvm::GlobalVariable(M, llvm::Type::getInt64Ty(F.getContext()), false, llvm::GlobalValue::LinkageTypes::PrivateLinkage, llvm::ConstantInt::get(llvm::Type::getInt64Ty(F.getContext()), blocks2index[b] * 8), "");
-                        auto *arrayStart = builder.CreatePointerCast(globBlockArray, llvm::Type::getInt64Ty(F.getContext()));
-                        auto *index = builder.CreateLoad(llvm::Type::getInt64Ty(F.getContext()), indexGlob);
+                        auto *indexGlob = new llvm::GlobalVariable(M, largestIntType, false, llvm::GlobalValue::LinkageTypes::PrivateLinkage, llvm::ConstantInt::get(largestIntType, blocks2index[b] * 8), "");
+                        auto *arrayStart = builder.CreatePointerCast(globBlockArray, largestIntType);
+                        auto *index = builder.CreateLoad(largestIntType, indexGlob);
                         auto *blockArrayPtrInt = builder.CreateAdd(arrayStart, index);
-                        auto *blockArrayPtr = builder.CreateIntToPtr(blockArrayPtrInt, llvm::Type::getInt64PtrTy(F.getContext()));
-                        auto *blockPtrInt = builder.CreateLoad(llvm::Type::getInt64Ty(F.getContext()), blockArrayPtr);
-                        auto *blockPtrIntVal = builder.CreateSub(blockPtrInt, llvm::ConstantInt::get(llvm::Type::getInt64Ty(F.getContext()), blocks2key[b]));
+                        auto *blockArrayPtr = builder.CreateIntToPtr(blockArrayPtrInt, largestIntType);
+                        auto *blockPtrInt = builder.CreateLoad(largestIntType, blockArrayPtr);
+                        auto *blockPtrIntVal = builder.CreateSub(blockPtrInt, llvm::ConstantInt::get(largestIntType, blocks2key[b]));
                         auto *blockPtr = builder.CreateIntToPtr(blockPtrIntVal, llvm::Type::getInt8PtrTy(F.getContext()));
                         successorAddress.push_back(blockPtr);
                     }
